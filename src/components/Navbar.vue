4f25c63fda9e284d384d2c7d2a740c18e99ba2d5
Martin Pruus <martin.pruus@tptlive.ee>
2025-10-14 23:05:30 +0300

    Add navbar

---
+++ src/components/Navbar.vue
@@ -0,0 +1,17 @@
+<template>
+    <nav class="navbar my-navbar" role="navigation" aria-label="main navigation">
+        <div class="navbar-brand">
+            <div class="navbar-item">
+                <p>TPT tagaisisde rakendus</p>
+            </div>
+        </div>
+        <div class="navbar-menu">
+            <div class="navbar-end">
+                <div class="navbar-item">
+                    <button class="button is-primary">Logi sisse</button>
+                    <button class="button">Registreeru</button>
+                </div>
+            </div>
+        </div>
+    </nav>
+</template>
