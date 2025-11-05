349c77deb9ed5d3978c2dd014205924890aac08a
Martin Pruus <martin.pruus@tptlive.ee>
2025-11-05 15:10:48 +0200

    Forgor semi colons

--- server/server.js
+++ server/server.js
@@ -3,9 +3,9 @@ const app = express();
 const port = 3000;
 
 app.get('/', (req, res) => {
-    res.send('Hello World!')
+    res.send('Hello World!');
 });
 
 app.listen(port, () => {
-    console.log(`Local:  http://localhost:${port}`)
+    console.log(`Local:  http://localhost:${port}`);
 });
